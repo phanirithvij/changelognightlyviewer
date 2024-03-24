658e4c992627a47af7749c5f5df5995916ee41e4
phanirithvij <phanirithvij2000@gmail.com>
2024-03-24 19:27:20 +0530

    basic htmx endpoint with echo framework

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+        "net/http"
+
+        "github.com/labstack/echo/v4"
+        "github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello nightlyviewer")
+        e := echo.New()
+
+        e.Use(middleware.Logger())
+        e.Use(middleware.Recover())
+        e.Use(middleware.Gzip())
+
+        e.File("/", "dist/index.html")
+	e.Static("/dist", "dist")
+
+        e.POST("/clicked", clickedP)
+
+        e.Logger.Fatal(e.Start(":8080"))
 }
+
+func clickedP(c echo.Context) error {
+        return c.HTML(http.StatusOK, `
+        <button id="baton" hx-post="/clicked" hx-swap="outerHTML">
+                Click Me Again
+        </button>`)
+}
+
